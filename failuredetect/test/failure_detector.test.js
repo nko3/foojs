5a2c01d5a1b3f682bd0dd0db1226b65a31f22c1b
Mikito Takada <mikito.takada@gmail.com>
2012-11-10 14:18:38 -0800

    Finish failure detector tests

--- failuredetect/test/failure_detector.test.js
+++ failuredetect/test/failure_detector.test.js
@@ -4,6 +4,7 @@ var assert = require('assert'),
 exports['given a server and a client'] = {
 
   beforeEach: function() {
+    var self = this;
     var server = this.server = {
       id: 1000,
       detector: new FailureDetector(100),
@@ -40,6 +41,7 @@ exports['given a server and a client'] = {
         switch(req) {
           case 'connect':
             client.detector.connected(server.id);
+            server.close = client.detector.ungracefulDisconnectFn(server.id);
             break;
           case 'disconnect':
             client.detector.disconnect(server.id);
@@ -51,16 +53,24 @@ exports['given a server and a client'] = {
         server.onRequest(client, 'disconnect');
       }
     }
+    this.clientNotifications = [];
+    this.serverNotifications = [];
+    this.server.detector.on('connect', function(id) {
+      self.serverNotifications.push({ op: 'connect', id: id});
+    });
+    this.server.detector.on('disconnect', function(id) {
+      self.serverNotifications.push({ op: 'disconnect', id: id});
+    });
+    this.client.detector.on('connect', function(id) {
+      self.clientNotifications.push({ op: 'connect', id: id});
+    });
+    this.client.detector.on('disconnect', function(id) {
+      self.clientNotifications.push({ op: 'disconnect', id: id});
+    });
   },
 
   'can detect when a client is gone gracefully': function() {
-    var notifications = [];
-    this.server.detector.on('connect', function(id) {
-      notifications.push({ op: 'connect', id: id});
-    });
-    this.server.detector.on('disconnect', function(id) {
-      notifications.push({ op: 'disconnect', id: id});
-    });
+    var notifications = this.serverNotifications, client = this.client;
     this.client.connect();
     assert.equal(notifications[0].op, 'connect');
     assert.equal(notifications[0].id, this.client.id);
@@ -72,13 +82,7 @@ exports['given a server and a client'] = {
 
   'can detect when a client is gone ungracefully due to timeout': function(done) {
     // e.g. client.close() is called to trigger an unexpected disconnect like a TCP close
-    var notifications = [], client = this.client;
-    this.server.detector.on('connect', function(id) {
-      notifications.push({ op: 'connect', id: id});
-    });
-    this.server.detector.on('disconnect', function(id) {
-      notifications.push({ op: 'disconnect', id: id});
-    });
+    var notifications = this.serverNotifications, client = this.client;
     this.client.connect();
     assert.equal(notifications[0].op, 'connect');
     assert.equal(notifications[0].id, this.client.id);
@@ -92,39 +96,50 @@ exports['given a server and a client'] = {
   },
 
   'can detect when a client is gone ungracefully from a connection drop': function() {
-    var notifications = [], client = this.client;
-    this.server.detector.on('connect', function(id) {
-      notifications.push({ op: 'connect', id: id});
-    });
-    this.server.detector.on('disconnect', function(id) {
-      notifications.push({ op: 'disconnect', id: id});
-    });
+    var notifications = this.serverNotifications, client = this.client;
     this.client.connect();
     assert.equal(notifications[0].op, 'connect');
-    assert.equal(notifications[0].id, this.client.id);
+    assert.equal(notifications[0].id, client.id);
     this.client.message();
     this.client.close(); // e.g. connection.on('close') is triggered
     assert.equal(notifications[1].op, 'disconnect');
     assert.equal(notifications[1].id, client.id);
   },
 
-  'can detect when a server does not respond to the connect in time': function() {
-    this.server.detector.on('connect', function(id) {
-      notifications.push({ op: 'connect', id: id});
-    });
-    this.server.detector.on('disconnect', function(id) {
-      notifications.push({ op: 'disconnect', id: id});
-    });
+  'can detect when a server does not respond to the connect in time': function(done) {
+    var notifications = this.clientNotifications, client = this.client, server = this.server;
+    // break the server's response
+    server.onRequest = function() {};
+    client.connect();
+    setTimeout(function() {
+      assert.equal(notifications[0].op, 'disconnect');
+      assert.equal(notifications[0].id, server.id);
+      done();
+    }, 210);
   },
 
-  'can detect when a server does not respond to a message in time': function() {
-
+  'can detect when a server does not respond to a message in time': function(done) {
+    var notifications = this.clientNotifications, client = this.client, server = this.server;
+    client.connect();
+    // break the server's response
+    server.onRequest = function() {};
+    client.message();
+    setTimeout(function() {
+      assert.equal(notifications[0].op, 'connect');
+      assert.equal(notifications[0].id, server.id);
+      assert.equal(notifications[1].op, 'disconnect');
+      assert.equal(notifications[1].id, server.id);
+      done();
+    }, 210);
   },
 
   'can detect when a server is gone ungracefully': function() {
-
+    var notifications = this.clientNotifications, client = this.client, server = this.server;
+    client.connect();
+    server.close();
+    assert.equal(notifications[1].op, 'disconnect');
+    assert.equal(notifications[1].id, server.id);
   }
-
 };
 
 // if this module is the script being run, then run the tests:
