bbe995e90837461ff3445642652c199a9830536d
Mikito Takada <mikito.takada@gmail.com>
2012-11-09 16:58:09 -0800

    Sketch out follower

--- zab/follower.js
+++ zab/follower.js
@@ -3,59 +3,161 @@
 # Algorithm 1: Zab Phase 1: Discovery.
 
 Follower F :
+
 Send the message FOLLOWERINFO(F.acceptedEpoch) to L
 upon receiving NEWEPOCH(e ) from L do
-if e > F.acceptedEpoch then
-F.acceptedEpoch ← e
-// stored to non-volatile memory
-Send ACKEPOCH(F.currentEpoch, F.history, F.lastZxid) to L
-goto Phase 2
-else if e < F.acceptedEpoch then
-F.state ← election and goto Phase 0 (leader election)
-end
+  if e > F.acceptedEpoch then
+    F.acceptedEpoch ← e // stored to non-volatile memory
+    Send ACKEPOCH(F.currentEpoch, F.history, F.lastZxid) to L
+    goto Phase 2
+  else if e < F.acceptedEpoch then
+    F.state ← election and goto Phase 0 (leader election)
+  end
 end
 
+*/
+
+function Follower() {
+  this.phase = 1;
+  this.acceptedEpoch = null;
+  this.currentEpoch = ???;
+  this.history = [];
+  this.lastZxid = ???;
+}
+
+Follower.prototype.discovery = function() {
+  // Send the message FOLLOWERINFO(F.acceptedEpoch) to L
+  this.send('leader', {
+    type: 'FOLLOWERINFO',
+    epoch: this.acceptedEpoch
+  });
+};
+
+Follower.prototype.discoveryMessage = function(m) {
+  if(m.type == 'NEWEPOCH') /* && m.source == 'leader' */ {
+    if(m.epoch > this.acceptedEpoch) {
+      this.acceptedEpoch = m.epoch;
+      // TODO flush to disk
+      this.send('leader', {
+        type: 'ACKEPOCH',
+        currentEpoch: this.currentEpoch,
+        history: this.history,
+        lastZxid: this.lastZxid
+      });
+      this.phase = 2;
+    }
+  }
+};
+
+Follower.prototype.message = function(m) {
+  switch(this.phase) {
+    case 1:
+      this.discoveryMessage(m);
+    case 2:
+      this.synchronization(m);
+    case 3:
+      this.broadcast(m);
+  }
+};
+
+/*
 # Algorithm 2: Zab Phase 2: Synchronization.
 
 Follower F :
 upon receiving NEWLEADER(e , H) from L do
-if F.acceptedEpoch = e then
-atomically
-F.currentEpoch ← e
-// stored to non-volatile memory
-for each v, z ∈ H, in order of zxids, do
-Accept the proposal e , v, z
-end
-F.history ← H
-// stored to non-volatile memory
-end
-Send an ACKNEWLEADER(e , H) to L
-else
-F.state ← election and goto Phase 0
-end
+  if F.acceptedEpoch = e then
+    atomically
+      F.currentEpoch ← e
+      // stored to non-volatile memory
+      for each v, z ∈ H, in order of zxids, do
+        Accept the proposal e , v, z
+      end
+      F.history ← H // stored to non-volatile memory
+    end
+    Send an ACKNEWLEADER(e , H) to L
+  else
+    F.state ← election and goto Phase 0
+  end
 end
 upon receiving COMMIT from L do
-for each outstanding transaction v, z ∈ F.history, in order of zxids, do
-Deliver v, z
+  for each outstanding transaction v, z ∈ F.history, in order of zxids, do
+    Deliver v, z
+  end
+  goto Phase 3
 end
-goto Phase 3
-end
+*/
 
+Follower.prototype.synchronization = function(m) {
+  var self = this;
+  if(m.type == 'NEWELADER') {
+    if(this.acceptedEpoch == m.epoch && m.history) {
+      // todo ATOMICALLY
+      this.currentEpoch = m.epoch; // TODO flush to disk
+      m.history.sort(function(a, b){
+        return b.zxid - a.zxid;
+      }).forEach(function(e) {
+        acceptProposal(e, e.z, e.v);
+      });
+      this.history = m.history;
+      // end ATOMICALLY
+      this.send('leader', {
+        type: 'ACKNEWLEADER',
+        epoch: this.currentEpoch,
+        history: m.history
+      });
+    }
+  }
+  if(m.type == 'COMMIT') {
+    this.history.outstandingTransactions().sort(function(a, b) {
+      return b.zxid - a.zxid;
+    }).forEach(function(t) {
+      self.emit('deliver', t.v, t.z);
+    });
+    this.state = 3;
+  }
+};
+
+/*
 # Algorithm 3: Zab Phase 3: Broadcast.
 
 Follower F :
 if F is leading then Invokes ready(e )
 upon receiving proposal e , v, z from L do
-Append proposal e , v, z to F.history
-Send ACK( e , v, z ) to L
+  Append proposal e , v, z to F.history
+  Send ACK( e , v, z ) to L
 end
 upon receiving COMMIT(e , v, z ) from L do
-while there is some outstanding transaction v , z ∈ F.history such that z
-Do nothing (wait)
-end
-Commit (deliver) transaction v, z
+  while there is some outstanding transaction v , z ∈ F.history such that z
+    Do nothing (wait)
+  end
+  Commit (deliver) transaction v, z
 end
 
 */
 
+Follower.prototype.broadcast = function(m) {
+  var self = this;
+  if (this.isLeader) {
+    this.emit('ready', this.currentEpoch);
+  }
+  if(m.type == 'PROPOSAL') {
+    this.history.push(m); // with write-ahead logging
+    this.send('leader', {
+      type: 'ACK',
+      v: m.v,
+      z: m.z
+    });
+  }
+  if(m.type == 'COMMIT') {
+    if(this.outstandingTransactions().length > 0) {
+      this.emit('deliver', t.v, t.z);
+    } else {
+      this.on('outstandingTransactionsDone', function() {
+        self.emit('deliver', t.v, t.z);
+      });
+    }
+  }
 
+};
+
+module.exports = Follower;
--- zab/leader.js
+++ zab/leader.js
@@ -4,48 +4,94 @@
 
 Leader L:
 upon receiving FOLLOWERINFO(e) messages from a quorum Q of connected followers do
-Make epoch number e such that e > e for all e received through FOLLOWERINFO(e)
-Propose NEWEPOCH(e ) to all followers in Q
+  Make epoch number e such that e > e for all e received through FOLLOWERINFO(e)
+  Propose NEWEPOCH(e ) to all followers in Q
 end
 upon receiving ACKEPOCH from all followers in Q do
-Find the follower f in Q such that for all f ∈ Q \ {f }:
-either f .currentEpoch < f.currentEpoch
-or (f .currentEpoch = f.currentEpoch) ∧ (f .lastZxid z f.lastZxid)
-L.history ← f.history
-// stored to non-volatile memory
-goto Phase 2
+  Find the follower f in Q such that for all fb ∈ Q \ {f }:
+  either fb .currentEpoch < f.currentEpoch
+  or (fb .currentEpoch = f.currentEpoch) ∧ (fb .lastZxid <= f.lastZxid)
+  L.history ← f.history
+  // stored to non-volatile memory
+  goto Phase 2
 end
+*/
 
+function Leader() {
+  this.phase = 1;
+  this.totalNodes = 3;
+  this.quorumSize = 2;
 
+  this.discoveryResponses = {};
+  this.ackResponses = {};
+}
+
+Leader.prototype.discovery = function(m) {
+  if(m.type == 'FOLLOWERINFO') {
+    this.discoveryResponses[m.senderId] = m;
+
+    if(Object.keys(this.discoveryResponses).length > this.quorumSize) {
+      var maxEpoch = Object.keys(this.discoveryResponses).reduce(function(prev, key){
+        return Math.max(prev, self.discoveryResponses[key].epoch);
+      }, 0);
+      this.broadcast({
+        type: 'NEWEPOCH',
+        epoch: maxEpoch
+      });
+    }
+  }
+  if(m.type == 'ACKEPOCH') {
+    this.ackResponses[m.senderId] = m;
+    if(Object.keys(this.ackResponses).length > this.quorumSize) {
+      // first, sort by currentEpoch
+      var sortedIds = Object.keys(this.ackResponses).sort(function(a, b) {
+        if(a.epoch - b.epoch != 0) {
+          return a.epoch - b.epoch;
+        } else {
+          return a.lastZxid - b.lastZxid;
+        }
+      });
+      var chosen = sortedIds[0];
+      this.history = this.ackResponses[chosen].history; // to non-volatile memory
+    }
+  }
+};
+
+/*
 # Algorithm 2: Zab Phase 2: Synchronization.
 
 Leader L:
 Send the message NEWLEADER(e , L.history) to all followers in Q
 upon receiving ACKNEWLEADER messages from some quorum of followers do
-Send a COMMIT message to all followers
-goto Phase 3
-6 end
+  Send a COMMIT message to all followers
+  goto Phase 3
+end
+*/
 
+Leader.prototype.synchronization = function(m) {
+
+};
+
+/*
 # Algorithm 3: Zab Phase 3: Broadcast.
 
 Leader L:
 upon receiving a write request v do
-Propose e , v, z to all followers in Q, where z = e , c , such that z succeeds all zxid
-values previously broadcast in e (c is the previous zxid’s counter plus an increment of one)
+  Propose e , v, z to all followers in Q, where z = e , c , such that z succeeds all zxid
+  values previously broadcast in e (c is the previous zxid’s counter plus an increment of one)
 end
 upon receiving ACK( e , v, z ) from a quorum of followers do
-Send COMMIT(e , v, z ) to all followers
+  Send COMMIT(e , v, z ) to all followers
 end
 // Reaction to an incoming new follower:
 upon receiving FOLLOWERINFO(e) from some follower f do
-Send NEWEPOCH(e ) to f
-Send NEWLEADER(e , L.history) to f
+  Send NEWEPOCH(e ) to f
+  Send NEWLEADER(e , L.history) to f
 end
 upon receiving ACKNEWLEADER from follower f do
-Send a COMMIT message to f
-Q ← Q ∪ {f }
+  Send a COMMIT message to f
+  Q ← Q ∪ {f }
 end
-
 
 */
 
