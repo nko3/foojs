b36d824f9ce35049f0cc23cdba3b539c312b0e42
Mikito Takada <mikito.takada@gmail.com>
2012-11-11 15:49:16 -0800

    Create project page, deploy to nodejitsu

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs'),
+
+    readme = fs.readFileSync('./index.html');
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(readme);
 }).listen(8000);
 
 console.log('Server running at http://0.0.0.0:8000/');
